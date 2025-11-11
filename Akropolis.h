ffb60f15ecc020b66565b71dd5bf27c25c1a5731
Samuel Bliard <samuel.bliard@etu.utc.fr>
2025-11-11 15:08:34 +0000

    mise de jour de la classe coordonnée elle contient : - toutes les surcharges nécéssaire - méthode pour trouver le voisin - méthode pour la distance entre deux hexagone

--- Akropolis.h
+++ Akropolis.h
@@ -172,9 +172,57 @@ pour rapporter des points. Il est possible de jouer avec plusieurs variantes dan
 
 
     
-    class CoordHex{
-        //coordonées axiales q, r,s avec s = -q -r
-        int q, r; 
+   class CoordHex {
+    private:
+        int q = 0;
+        int r = 0;
+    public:
+        //constructeur
+        CoordHex() : q(0), r(0) {}
+        CoordHex(int q_, int r_) : q(q_), r(r_) {}
+        ~CoordHex();
+
+        // Coordonées axiales donc <s, q, r>
+        // s est implicite : s = -q - r
+        int s() const { return -q - r; }
+
+        // Surcharges opérateurs
+        CoordHex operator+(const CoordHex& autre) const {
+            return CoordHex(q + autre.q, r + autre.r);
+        }
+        
+        CoordHex operator-(const CoordHex& autre) const {
+            return CoordHex(q - autre.q, r - autre.r);
+        }
+        
+        bool operator==(const CoordHex& autre) const {
+            return q == autre.q && r == autre.r;
+        }
+        bool operator!=(const CoordHex& autre) const {
+            return !(*this == autre);
+        }
+        // cet opérateur sera utilisé dans le tri
+        bool operator<(const CoordHex& autre) const {
+            if (q != autre.q) return q < autre.q;
+            return r < autre.r;
+        }
+        
+
+        // Donne le voisin à une direction
+        CoordHex voisin(int direction) const {
+            static const std::array<CoordHex, 6> directions = {{
+                CoordHex(1, 0), CoordHex(0, 1), CoordHex(-1, 1),
+                CoordHex(-1, 0), CoordHex(0, -1), CoordHex(1, -1)
+            }};
+            return *this + directions[direction % 6]; //utilisation de la surcharge +
+        }
+
+        // Distance entre deux hexagones
+        int distance(const CoordHex& autre) const {
+            CoordHex diff = *this - autre;
+            // Formule de RedBlobGames (convertie de cubique)
+            return (std::abs(diff.q) + std::abs(diff.r) + std::abs(diff.s())) / 2;
+        }
     };
     
     
