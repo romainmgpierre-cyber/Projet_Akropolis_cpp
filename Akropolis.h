5c08ca60cd01737a02fe910cbaf4cfdee60a74c8
Antoine LAVALLEE <antoine.lavallee@etu.utc.fr>
2025-11-12 16:40:32 +0100

    Ré implémentation de l'utilisation des pierres suite à une erreur de commit

--- Akropolis.h
+++ Akropolis.h
@@ -166,7 +166,6 @@ pour rapporter des points. Il est possible de jouer avec plusieurs variantes dan
             unsigned int hauteur=1;
             bool proprietaire; //true si possède des haxagones (pour la gestion de la mémoire)
             
-            
         public : 
             TuileCite(size_t id, HexagoneConstruction* h1, 
               HexagoneConstruction* h2, HexagoneConstruction* h3,
@@ -175,7 +174,6 @@ pour rapporter des points. Il est possible de jouer avec plusieurs variantes dan
             TuileCite(const TuileCite&)=delete;
             TuileCite& operator=(const TuileCite& )=delete;
 
-            
             size_t getId() const { return id; }
             const array<HexagoneConstruction*, 3>& getHexagones() const { 
                 return hexagones; 
@@ -351,6 +349,14 @@ pour rapporter des points. Il est possible de jouer avec plusieurs variantes dan
         ChoixTuile(size_t id) : id(id) {
             tuilesDisponibles.reserve(MAX_TUILES);
         }
+        
+        // --- AJOUTS ICI ---
+        size_t calculerCout(size_t index) const {
+            return index;
+        }
+
+        TuileCite* choisirTuile(Joueur* joueur, size_t index);
+        // ------------------
         
         bool ajouterTuile(TuileCite* tuile);
         bool retirerTuile(size_t tuileId);
