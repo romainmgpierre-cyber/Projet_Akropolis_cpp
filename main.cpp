94d821edc64bd5dddde41551b2ea7a8979fcc8f9
Louise Ancely <louise.ancely@etu.utc.fr>
2025-12-03 10:36:12 +0000

    main version qt en commentaire à la fin

--- main.cpp
+++ main.cpp
@@ -250,3 +250,16 @@ int main() {
 
     return 0;
 }
+
+/*
+#include "MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+*/
