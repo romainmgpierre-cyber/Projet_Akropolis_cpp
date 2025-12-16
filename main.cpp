ea96b129205286fe898bc11efc95b0eaa91a1365
Romain Pierre <romain.pierre@etu.utc.fr>
2025-12-16 17:15:38 +0000

    Permet a l'utilisateur de choisir son mode de jeu

--- main.cpp
+++ main.cpp
@@ -8,8 +8,6 @@
 #include "Pioche_ChoixTuile.h"
 #include "Score.h"
 #include "Type.h"
-// #include "MainWindow.h"
-//#include "HexGridWidget.h"
 #include <iostream>
 #include <string>
 #include <limits>
@@ -20,68 +18,52 @@
 #include "MenuConfiguration.h"
 #include "FonctionsAide.h"
 #include "Sauvegarde.h"
+#include "MainWindow.h" // Nécessaire pour LancerQt
+#include <QApplication> // Nécessaire pour LancerQt
 
 using namespace std;
 using namespace Akropolis;
-/*
 
-// Fonction de lancement d'une nouvelle partie
 void LancerPartie(const Akropolis::Configuration& config) {
-    // Création de la partie en utilisant les donnees de 'config'
     Akropolis::Partie partie(1, config.getMode());
     partie.setDifficulte(config.getDifficulte());
 
-    // Boucle pour ajouter les joueurs humains
     for (size_t i = 0; i < config.getNomsJoueurs().size(); ++i) {
         partie.ajouterJoueur(config.getNomsJoueurs()[i], i + 1);
     }
 
-    // Ajout de l'IA si mode solo
     if (config.getMode() == Akropolis::ModeJeu::SOLO) {
         partie.ajouterJoueur("Illustre Constructeur", 2, true);
     }
 
-    // Lancement de la boucle de jeu
     partie.lancerPartie();
-
-    // Affichage des resultats
     AfficherResultats(partie);
 }
 
-
-int main() {
-    // Initialisation de la graine aléatoire
+int LancerConsole() {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
 
     try {
         int choixMenu;
         MenuConfiguration menuConfig;
         while (true) {
-            // Affichage du menu
             cout << "\n--- MENU PRINCIPAL ---" << endl;
             cout << "[1] Créer une nouvelle partie" << endl;
-            cout << "[2] Charger une partie enregistrée (pas encore implémenté)" << endl;
+            cout << "[2] Charger une partie enregistrée" << endl;
             cout << "[3] Quitter le jeu" << endl;
 
-            // Cas où on quite le jeu
             if (!readIntOrQuit(choixMenu, 1, 3, "Votre choix")) {
                 choixMenu = 3;
             }
             if (choixMenu == 1) {
-                // configuration de la partie
                 Configuration config = menuConfig.demarrerConfiguration();
-                // Lancement de la partie
                 LancerPartie(config);
             } else if (choixMenu == 2) {
-                // on charge le menu des sauvegardes
                 Sauvegarde::MenuSauvegardes();
-                //Sauvegarde::EnregistrerPartie();
             } else if (choixMenu == 3) {
                 cout << "\n A bientot !" << endl;
                 break;
             }
-
         }
     } catch (const PartieAnnulee& e) {
         cout << "\n\n=== FIN DE PARTIE ===" << endl;
@@ -94,19 +76,36 @@ int main() {
         cerr << "\n ERREUR SYSTEME: " << e.what() << endl;
         return 1;
     }
-
     return 0;
 }
 
-*/
-#include "MainWindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
+int LancerQt(int argc, char *argv[])
 {
-
-     QApplication a(argc, argv);
-     MainWindow w;
-     w.show();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
     return a.exec();
+}
+
+int main(int argc, char *argv[]) {
+    int modeDeJeu;
+
+    while (true) {
+        cout << "Veuillez choisir le mode de jeu (Console : 1; Qt : 2) : ";
+        if (cin >> modeDeJeu) {
+            if (modeDeJeu == 1) {
+                return LancerConsole();
+            }
+            else if (modeDeJeu == 2) {
+                return LancerQt(argc, argv);
+            }
+        }
+
+        // Gestion erreur de saisie
+        cout << "Choix invalide." << endl;
+        cin.clear(); // Réinitialiser l'état d'erreur
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Vider le buffer
+    }
+
+    return 0;
 }
